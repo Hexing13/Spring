3802f2599943c109eab845adbdbdca1cfe583b1f
Hexing13 <vicky1776898728@gmail.com>
2015-08-22 09:37:02 +0800

    spring的学习

--- src/com/hx/dao/Mysql.java
+++ src/com/hx/dao/Mysql.java
@@ -3,15 +3,38 @@ package com.hx.dao;
 import com.hx.model.User;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Created by hexing on 15-8-19.
  */
 @Component("mysql")
 public class Mysql implements UserDao{
 
+    private DataSource dataSource;
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Resource
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void save(User u) {
-        System.out.println("存入Mysql数据库!");
+        try {
+            Connection conn = dataSource.getConnection();
+            conn.createStatement().executeUpdate("insert into spring values(null,'zhangsan')");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("存入Mysql数据库");
     }
 
 }
